2d388ff48de716ba7b20440559b45260189d411f
1313ou <1313ou@gmail.com>
2024-05-18 08:50:24 +0200

    both sequence and iteration

--- src/main/java/org/oewntk/wndb/out/SynsetProcessor.kt
+++ src/main/java/org/oewntk/wndb/out/SynsetProcessor.kt
@@ -3,10 +3,7 @@
  */
 package org.oewntk.wndb.out
 
-import org.oewntk.model.Category
-import org.oewntk.model.Lex
-import org.oewntk.model.Sense
-import org.oewntk.model.Synset
+import org.oewntk.model.*
 import org.oewntk.wndb.out.Coder.codeFrameId
 import org.oewntk.wndb.out.Coder.codeLexFile
 import org.oewntk.wndb.out.Data.AdjMember
@@ -66,7 +63,7 @@ protected constructor(
     /**
      * Intermediate class to detect duplicates
      */
-    internal class RelationData(private val isSenseRelation: Boolean, val relType: String, val target: String) : Comparable<RelationData> {
+    internal class RelationData(private val isSenseRelation: Boolean, val relation: String, val target: String) : Comparable<RelationData> {
 
         // identity
         override fun equals(other: Any?): Boolean {
@@ -77,11 +74,11 @@ protected constructor(
                 return false
             }
             val that = other as RelationData
-            return isSenseRelation == that.isSenseRelation && relType == that.relType && target == that.target
+            return isSenseRelation == that.isSenseRelation && relation == that.relation && target == that.target
         }
 
         override fun hashCode(): Int {
-            return Objects.hash(isSenseRelation, relType, target)
+            return Objects.hash(isSenseRelation, relation, target)
         }
 
         // order
@@ -90,16 +87,16 @@ protected constructor(
             if (c != 0) {
                 return c
             }
-            c = relType.compareTo(other.relType)
+            c = relation.compareTo(other.relation)
             if (c != 0) {
                 return c
             }
-            return relType.compareTo(other.relType)
+            return relation.compareTo(other.relation)
         }
 
         // string
         override fun toString(): String {
-            return (if (this.isSenseRelation) "SenseRelation" else "SynsetRelation") + " relType=" + this.relType + " target=" + this.target
+            return (if (this.isSenseRelation) "SenseRelation" else "SynsetRelation") + " relType=" + this.relation + " target=" + this.target
         }
     }
 
@@ -145,9 +142,6 @@ protected constructor(
      * ```
      */
     fun getData(synset: Synset, offset: Long): String {
-        // init
-        val relations: MutableList<Data.Relation> = ArrayList()
-        val frames = Data.Frames()
 
         // attribute data
         val type = synset.type
@@ -167,7 +161,133 @@ protected constructor(
         val lexfileNum = buildLexfileNum(synset)
 
         // synset relations
+        val synsetRelations = getSynsetRelationData(synset, offset)
+        val synsetRelations0 = getSynsetRelationData0(synset, offset)
+        assert(synsetRelations == synsetRelations0) { "\n$synsetRelations\n$synsetRelations0" }
+
+        // senses that have this synset as target in "synset" field
+        val senses = synset.findSenses(
+            { lemma -> lexesByLemma[lemma]!! },
+            { senseKey -> sensesById[senseKey]!! },
+        )
+        assert(senses.isNotEmpty())
+
+        // sense relations
+        val senseRelations = getSenseRelationData(synset, senses, offset)
+        val senseRelations0 = getSenseRelationData0(synset, senses, offset)
+        assert(senseRelations == senseRelations0) { "\n$senseRelations\n$senseRelations0" }
+
+        // verb frames
+        val verbFrames = getVerbFrames(senses)
+        val verbFrames0 = getVerbFrames0(senses)
+        assert(verbFrames == verbFrames0) { "\n$verbFrames\n$verbFrames0" }
+
+        // assemble
+        val lexIdCompat = (flags and Flags.LEXID_COMPAT) != 0
+        val membersData = members.joinToStringWithCount(separator = " ", countSeparator = " ", countFormat = ::intFormatHex2x) { it.toWndbString(lexIdCompat) }
+        val allRelations = synsetRelations + senseRelations
+        val relatedData = allRelations.joinToStringWithCount(separator = " ", countSeparator = " ", countFormat = ::intFormat3) { it.toWndbString() }
+        var verbframesData = verbFrames.toWndbString(type, members.size)
+        if (verbframesData.isNotEmpty()) {
+            verbframesData = " $verbframesData"
+        }
+        val definitionsData = definitions.joinToString("; ")
+        val examplesData = if (examples.isNullOrEmpty()) "" else "; " + examples.joinToString(separator = " ") { example -> "\"$example\"" }
+        return "${offsetFormat(offset)} ${intFormat2(lexfileNum)} $type $membersData $relatedData$verbframesData | $definitionsData$examplesData  \n"
+    }
+
+    private fun getSynsetRelationData(synset: Synset, offset: Long): List<Data.Relation> {
+
+        return if (synset.relations.isNullOrEmpty()) emptyList() else {
+            val pointerCompat = (flags and Flags.POINTER_COMPAT) != 0
+            synset.relations!!
+                .asSequence()
+                .flatMap { (relationType, targetSynsets) ->
+                    targetSynsets
+                        .asSequence()
+                        .map { targetSenseId -> RelationData(false, relationType, targetSenseId) }
+                }
+                .toSetWarnDuplicates { duplicate ->
+                    if (LOG_DUPLICATE_RELATION && log()) {
+                        Tracing.psErr.println("[W] Synset ${synset.synsetId} has duplicate ${duplicate.relation}")
+                    }
+                }.mapNotNull { relationData ->
+                    val targetSynset = synsetsById[relationData.target]
+
+                    val targetOffset = offsetFunction.invoke(relationData.target)
+                    val targetType = targetSynset!!.type
+                    try {
+                        Data.Relation(relationData.relation, synset.type, targetType, targetOffset, 0, 0, pointerCompat)
+
+                    } catch (e: CompatException) {
+                        val cause = e.cause!!.message!!
+                        val count = incompats.computeIfAbsent(cause) { 0 } + 1
+                        incompats[cause] = count
+                        null
+                    } catch (e: IllegalArgumentException) {
+                        if (LOG_DISCARDED && log()) {
+                            Tracing.psErr.println("[W] Discarded relation '${relationData.relation}' synset=${synset.synsetId} offset=$offset")
+                        }
+                        throw e
+                    }
+                }
+                .toList()
+        }
+    }
+
+    private fun getSenseRelationData(synset: Synset, senses: List<Sense>, offset: Long): List<Data.Relation> {
+        return senses
+            .asSequence()
+            .filter { sense -> !sense.relations.isNullOrEmpty() }
+            .flatMap { sense ->
+                sense.relations!!
+                    .asSequence()
+                    .map { entry: Map.Entry<Relation, MutableSet<SenseKey>> -> sense to entry }
+            }
+            .flatMap { (sense: Sense, relationEntry: Map.Entry<Relation, MutableSet<SenseKey>>) ->
+                val relation: Relation = relationEntry.key
+                val targetSynsets: MutableSet<SenseKey> = relationEntry.value
+                targetSynsets
+                    .asSequence()
+                    .map { targetSenseId: SenseKey -> sense to RelationData(true, relation, targetSenseId) }
+            }
+            .toSetWarnDuplicates { (sense: Sense, duplicate: RelationData) ->
+                if (LOG_DUPLICATE_RELATION && log()) {
+                    Tracing.psErr.println("[W] Sense ${sense.senseKey} has duplicate ${duplicate.relation}")
+                }
+            }
+            .mapNotNull { (sense: Sense, relationData: RelationData) ->
+                val targetSense = sensesById[relationData.target]!!
+                val targetSynsetId = targetSense.synsetId
+                val targetSynset = synsetsById[targetSynsetId]!!
+
+                val memberNum = synset.findIndexOfMember(sense.lemma) + 1
+
+                try {
+                    buildSenseRelation(relationData.relation, synset.type, memberNum, targetSense, targetSynset, targetSynsetId)
+
+                } catch (e: CompatException) {
+                    val cause = e.cause!!.message!!
+                    val count = incompats.computeIfAbsent(cause) { 0 } + 1
+                    incompats[cause] = count
+                    null
+                } catch (e: IllegalArgumentException) {
+                    if (LOG_DISCARDED && log()) {
+                        Tracing.psErr.println("[W] Discarded relation '${relationData.relation}' synset=${synset.synsetId} offset=$offset")
+                    }
+                    null
+                    // throw e;
+                }
+            }
+            .toList()
+    }
+
+    private fun getSynsetRelationData0(synset: Synset, offset: Long): List<Data.Relation> {
+
+        val relations: MutableList<Data.Relation> = ArrayList()
         if (!synset.relations.isNullOrEmpty()) {
+            val pointerCompat = (flags and Flags.POINTER_COMPAT) != 0
+
             val relationDataSet: MutableSet<RelationData> = LinkedHashSet()
             for ((relationType, values) in synset.relations!!) {
                 for (targetSynsetId in values) {
@@ -178,7 +298,6 @@ protected constructor(
                     }
                 }
             }
-            val pointerCompat = (flags and Flags.POINTER_COMPAT) != 0
             for (relationData in relationDataSet) {
                 val targetSynset = synsetsById[relationData.target]
 
@@ -186,7 +305,7 @@ protected constructor(
                 val targetType = targetSynset!!.type
                 var relation: Data.Relation
                 try {
-                    relation = Data.Relation(relationData.relType, type, targetType, targetOffset, 0, 0, pointerCompat)
+                    relation = Data.Relation(relationData.relation, synset.type, targetType, targetOffset, 0, 0, pointerCompat)
                 } catch (e: CompatException) {
                     val cause = e.cause!!.message!!
                     val count = incompats.computeIfAbsent(cause) { 0 } + 1
@@ -194,22 +313,98 @@ protected constructor(
                     continue
                 } catch (e: IllegalArgumentException) {
                     if (LOG_DISCARDED && log()) {
-                        Tracing.psErr.println("[W] Discarded relation '${relationData.relType}' synset=${synset.synsetId} offset=$offset")
+                        Tracing.psErr.println("[W] Discarded relation '${relationData.relation}' synset=${synset.synsetId} offset=$offset")
                     }
                     throw e
                 }
                 relations.add(relation)
             }
         }
+        return relations
+    }
 
-        // senses that have this synset as target in "synset" field
-        val senses = synset.findSenses(
-            { lemma -> lexesByLemma[lemma]!! },
-            { senseKey -> sensesById[senseKey]!! },
-        )
-        assert(senses.isNotEmpty())
+    private fun getSenseRelationData0(synset: Synset, senses: List<Sense>, offset: Long): List<Data.Relation> {
+
+        val relations: MutableList<Data.Relation> = ArrayList()
+        for (sense in senses) {
+
+            // sense relations
+            if (!sense.relations.isNullOrEmpty()) {
+                val lemma = sense.lemma
+
+                val senseRelationDataSet = sense.relations!!
+                    .asSequence()
+                    .flatMap { (relationType, targetSynsets) ->
+                        targetSynsets
+                            .asSequence()
+                            .map { targetSenseId -> RelationData(true, relationType, targetSenseId) }
+                    }
+                    .toSetWarnDuplicates { duplicate ->
+                        if (LOG_DUPLICATE_RELATION && log()) {
+                            Tracing.psErr.println("[W] Sense ${sense.senseKey} has duplicate ${duplicate.relation}")
+                        }
+                    }
+
+                for (relationData in senseRelationDataSet) {
+                    val targetSense = sensesById[relationData.target]!!
+                    val targetSynsetId = targetSense.synsetId
+                    val targetSynset = synsetsById[targetSynsetId]!!
+
+                    val memberNum = synset.findIndexOfMember(lemma) + 1
+
+                    try {
+                        val relation = buildSenseRelation(relationData.relation, synset.type, memberNum, targetSense, targetSynset, targetSynsetId)
+                        relations.add(relation)
+
+                    } catch (e: CompatException) {
+                        val cause = e.cause!!.message!!
+                        val count = incompats.computeIfAbsent(cause) { 0 } + 1
+                        incompats[cause] = count
+                    } catch (e: IllegalArgumentException) {
+                        if (LOG_DISCARDED && log()) {
+                            Tracing.psErr.println("[W] Discarded relation '${relationData.relation}' synset=${synset.synsetId} offset=$offset")
+                        }
+                        // throw e;
+                    }
+                }
+            }
+        }
+        return relations
+    }
+
+    private fun getVerbFrames(senses: List<Sense>): Data.Frames {
+
+        val verbFrameCompat = (flags and Flags.VERBFRAME_COMPAT) != 0
+        val frames = Data.Frames()
+        for (sense in senses) {
+
+            // verb frames attribute
+            if (!sense.verbFrames.isNullOrEmpty()) {
+                sense.verbFrames!!
+                    .asSequence()
+                    .map { verbframeId ->
+                        try {
+                            val code = codeFrameId(verbframeId, verbFrameCompat)
+                            Data.Frame(code, sense.findSynsetIndex(synsetsById) + 1)
+
+                        } catch (e: CompatException) {
+                            val cause = e.cause!!.message!!
+                            val count = incompats.computeIfAbsent(cause) { 0 } + 1
+                            incompats[cause] = count
+                            null
+                        }
+                    }
+                    .filterNotNull()
+                    .forEach { frames.add(it) }
+            }
+        }
+        return frames
+    }
+
+    private fun getVerbFrames0(senses: List<Sense>): Data.Frames {
 
         // iterate senses
+        val frames = Data.Frames()
         for (sense in senses) {
             val verbFrameCompat = (flags and Flags.VERBFRAME_COMPAT) != 0
 
@@ -232,69 +427,8 @@ protected constructor(
                     .filterNotNull()
                     .forEach { frames.add(it) }
             }
-
-            // sense relations
-            if (!sense.relations.isNullOrEmpty()) {
-                val lemma = sense.lemma
-
-                val senseRelationDataSet: MutableSet<RelationData> = LinkedHashSet()
-                for ((relationType, values) in sense.relations!!) {
-                    for (targetSenseId in values) {
-                        val relation = RelationData(true, relationType, targetSenseId)
-                        val wasThere = !senseRelationDataSet.add(relation)
-                        if (wasThere && LOG_DUPLICATE_RELATION && log()) {
-                            Tracing.psErr.println("[W] Sense ${sense.senseKey} has duplicate $relation")
-                        }
-                    }
-                }
-                val senseRelationDataSet2 = sense.relations!!
-                    .asSequence()
-                    .flatMap { (relationType, targetSynsets) ->
-                        targetSynsets
-                            .asSequence()
-                            .map { targetSenseId -> RelationData(true, relationType, targetSenseId) }
-                    }
-                    .toSet()
-
-                for (relationData in senseRelationDataSet) {
-                    val targetSense = sensesById[relationData.target]!!
-                    val targetSynsetId = targetSense.synsetId
-                    val targetSynset = synsetsById[targetSynsetId]!!
-
-                    val memberNum = synset.findIndexOfMember(lemma) + 1
-
-                    var relation: Data.Relation
-                    try {
-                        relation = buildSenseRelation(relationData.relType, type, memberNum, targetSense, targetSynset, targetSynsetId)
-                    } catch (e: CompatException) {
-                        val cause = e.cause!!.message!!
-                        val count = incompats.computeIfAbsent(cause) { 0 } + 1
-                        incompats[cause] = count
-                        continue
-                    } catch (e: IllegalArgumentException) {
-                        if (LOG_DISCARDED && log()) {
-                            Tracing.psErr.println("[W] Discarded relation '${relationData.relType}' synset=${synset.synsetId} offset=$offset")
-                        }
-                        // throw e;
-                        continue
-                    }
-                    relations.add(relation)
-                }
-            }
         }
-
-        // assemble
-        val lexIdCompat = (flags and Flags.LEXID_COMPAT) != 0
-        val membersData = members.joinToStringWithCount(separator = " ", countSeparator = " ", countFormat = ::intFormatHex2x) { it.toWndbString(lexIdCompat) }
-        val relatedData = relations.joinToStringWithCount(separator = " ", countSeparator = " ", countFormat = ::intFormat3) { it.toWndbString() }
-
-        var verbframesData = frames.toWndbString(type, members.size)
-        if (verbframesData.isNotEmpty()) {
-            verbframesData = " $verbframesData"
-        }
-        val definitionsData = definitions.joinToString("; ")
-        val examplesData = if (examples.isNullOrEmpty()) "" else "; " + examples.joinToString(separator = " ") { example -> "\"$example\"" }
-        return "${offsetFormat(offset)} ${intFormat2(lexfileNum)} $type $membersData $relatedData$verbframesData | $definitionsData$examplesData  \n"
+        return frames
     }
 
     /**
